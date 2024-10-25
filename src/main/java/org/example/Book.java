d985f6baddb68532d88b98def23166a6d0ec9378
RRam2020 <reecerram@Gmail.com>
2024-10-24 22:16:16 -0700

    test 004

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -1,8 +1,30 @@
+/**
+ * Represents a book
+ */
+
+
 package org.example;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
 public class Book {
-    private int id;
+    public static final int ISBN_=0;
+    public static final int TITLE_=0;
+    public static final int SUBJECT_=0;
+    public static final int PAGE_COUNT_=0;
+    public static final int AUTHOR_=0;
+    public static final int DUE_DATE_=0;
+
+    private String isbn;
     private String title;
-    private String text;
+    private String subject;
+    private int pageCount;
+    private String author;
+    private LocalDate dueDate;
+
+
+
+
 
 }
